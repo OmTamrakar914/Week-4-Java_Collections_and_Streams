5e2ec692b8bce80975dcfd4a63a113bc01c2d336
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-02-19 01:39:34 +0530

    [Om] Add. completed Reflection - generate json representation and done testing

---
+++ Day6-Java-Annotations-and-Reflection/src/main/java/reflection/advance_level/generate_json_representation/Person.java
@@ -0,0 +1,13 @@
+package reflection.advance_level.generate_json_representation;
+
+public class Person {
+    private String name;
+    private int age;
+    private boolean isEmployed;
+
+    public Person(String name, int age, boolean isEmployed) {
+        this.name = name;
+        this.age = age;
+        this.isEmployed = isEmployed;
+    }
+}
