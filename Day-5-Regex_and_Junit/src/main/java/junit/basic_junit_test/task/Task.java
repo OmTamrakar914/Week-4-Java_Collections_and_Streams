be52fe2fa216acff44776c24348cd37ff3a7f0a9
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-02-19 00:20:28 +0530

    [Om] Add. Junit - completed Date formatter and done testing

--- Day-5-Regex_and_Junit/src/main/java/junit/basic_junit_test/task/Task.java
+++
@@ -1,8 +0,0 @@
-package junit.basic_junit_test.task;
-
-public class Task {
-    public String longRunningTask() throws InterruptedException {
-        Thread.sleep(3000);
-        return "Task Completed";
-    }
-}
