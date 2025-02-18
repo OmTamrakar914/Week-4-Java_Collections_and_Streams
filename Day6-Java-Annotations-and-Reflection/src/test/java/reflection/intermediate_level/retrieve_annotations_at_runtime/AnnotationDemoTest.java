ef13a59b49c624170218febc1ed2b0a6cd6edef7
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-02-19 01:43:51 +0530

    [Om] Add. completed Reflection - retrieve annotation at runtime and done testing

---
+++ Day6-Java-Annotations-and-Reflection/src/test/java/reflection/intermediate_level/retrieve_annotations_at_runtime/AnnotationDemoTest.java
@@ -0,0 +1,21 @@
+package reflection.intermediate_level.retrieve_annotations_at_runtime;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnnotationDemoTest {
+
+    @Test
+    void testRetrieveAnnotationAtRuntime() {
+        // Given
+        Class<MyClass> obj = MyClass.class;
+
+        // When
+        Author annotation = obj.getAnnotation(Author.class);
+
+        // Then
+        assertNotNull(annotation, "The @Author annotation should be present.");
+        assertEquals("John Doe", annotation.name(), "The @Author annotation's name should be 'John Doe'.");
+    }
+}
