b817f890669f89adf73e949dd9134f399c0bb35e
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-02-19 00:24:09 +0530

    [Om] Add. Junit - completed temperature converter and done testing

---
+++ Day-5-Regex_and_Junit/src/main/java/junit/advance_junit_test/temperature_converter/TemperatureConverter.java
@@ -0,0 +1,14 @@
+package junit.advance_junit_test.temperature_converter;
+
+public class TemperatureConverter {
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+}
---
+++ Day-5-Regex_and_Junit/src/test/java/junit/advance_junit_test/temperature_converter/TemperatureConverterTest.java
@@ -0,0 +1,23 @@
+package junit.advance_junit_test.temperature_converter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TemperatureConverterTest {
+
+    @Test
+    void testCelsiusToFahrenheit() {
+        assertEquals(32.0, TemperatureConverter.celsiusToFahrenheit(0), 0.01);
+        assertEquals(212.0, TemperatureConverter.celsiusToFahrenheit(100), 0.01);
+        assertEquals(-40.0, TemperatureConverter.celsiusToFahrenheit(-40), 0.01);
+    }
+
+    @Test
+    void testFahrenheitToCelsius() {
+        assertEquals(0.0, TemperatureConverter.fahrenheitToCelsius(32), 0.01);
+        assertEquals(100.0, TemperatureConverter.fahrenheitToCelsius(212), 0.01);
+        assertEquals(-40.0, TemperatureConverter.fahrenheitToCelsius(-40), 0.01);
+    }
+
+}
